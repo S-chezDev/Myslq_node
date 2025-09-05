def7f278bc5f40197a16426d1fa5cbb71ce99253
S-chezDev <assanchez@soy.sena.edu.co>
2025-09-05 13:13:42 -0500

    actual cprrejido el puerto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app.js'
 import {PORT} from './config.js'
 
-app.listen(PORT)
-console.log('server running on port', PORT)
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
